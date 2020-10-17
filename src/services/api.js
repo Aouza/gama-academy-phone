4c1784384b1ea75f198b5f3f18f158b937b31255
= <alison-cruz@hotmail.com>
2020-10-17 19:01:01 -0300

    Service api add

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,8 @@ export const buttonsValues = [
   { number: 4, type: ["g", "h", "i"] },
   { number: 5, type: ["j", "k", "l"] },
   { number: 6, type: ["m", "n", "o"] },
-  { number: 7, type: ["p", "q", "r"] },
-  { number: 8, type: ["s", "t", "u"] },
-  { number: 9, type: ["v", "w", "x"] },
-  { number: 0, type: ["y", "z", ""] },
+  { number: 7, type: ["p", "q", "r", "s"] },
+  { number: 8, type: ["t", "u", "v"] },
+  { number: 9, type: ["w", "x", "y", "z"] },
+  { number: 0, type: ["", "", ""] },
 ];
